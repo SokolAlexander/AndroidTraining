3a323b9c2bd7da72036fc24f67f41383e5f6bf2d
SokolAlexander <asokol1994@gmail.com>
2021-01-30 20:44:02 +0300

    Text watcher, fragment

---
+++ AndersenTraning/app/src/main/java/com/example/andersentraning/FirstFragment.java
@@ -0,0 +1,21 @@
+package com.example.andersentraning;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+
+public class FirstFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.first_fragment,
+                container, false);
+
+
+        return view;
+    }
+}
--- AndersenTraning/app/src/main/java/com/example/andersentraning/MainActivity.java
+++ AndersenTraning/app/src/main/java/com/example/andersentraning/MainActivity.java
@@ -3,12 +3,66 @@ package com.example.andersentraning;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.andersentraning.FirstFragment;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText editText;
+    private TextView textView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.fragment_container_view, FirstFragment.class, null)
+                    .commit();
+        }
+        this.editText = findViewById(R.id.editText);
+        this.textView = findViewById(R.id.textView);
+        this.button = findViewById(R.id.button2);
+
+        button.setOnClickListener(this);
+        editText.addTextChangedListener(textWatcher);
+    }
+
+    private final TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            Log.d("textWatcher", s.toString());
+            textView.setText(s.toString());
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            Log.d("textAfter", s.toString());
+        }
+    };
+
+    private void showToast() {
+        Toast toast = Toast.makeText(getApplicationContext(),
+                textView.getText(), Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void onClick(View view) {
+        showToast();
     }
 }
