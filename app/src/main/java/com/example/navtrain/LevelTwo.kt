dd008550950ff52b533d83ed3ba607eaaf024ef5
SokolAlexander <asokol1994@gmail.com>
2021-03-16 15:02:49 +0300

    Nav train animations

--- app/src/main/java/com/example/navtrain/LevelTwo.kt
+++ app/src/main/java/com/example/navtrain/LevelTwo.kt
@@ -4,26 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 
-class HomeFragment: Fragment() {
+class LevelTwo: Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.home_fragment, container, false);
+        return inflater.inflate(R.layout.level_two, container, false);
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        val button = view.findViewById<Button>(R.id.nav_button)
-
-        button.setOnClickListener {
-            findNavController().navigate(R.id.action_home_dest_to_level_two, null)
-        }
     }
 }
--- app/src/main/java/com/example/navtrain/MainActivity.kt
+++ app/src/main/java/com/example/navtrain/MainActivity.kt
@@ -10,10 +10,7 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.onNavDestinationSelected
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.*
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -33,6 +30,7 @@ class MainActivity : AppCompatActivity() {
 
         drawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
+
         val host: NavHostFragment = supportFragmentManager
             .findFragmentById(R.id.my_nav_host_fragment) as NavHostFragment? ?: return
         navController = host.navController
@@ -41,40 +39,10 @@ class MainActivity : AppCompatActivity() {
 
         setupActionBarWithNavController(navController!!, appBarConfiguration!!)
         navView.setupWithNavController(navController)
-
-//
-//         val toolbar = findViewById<Toolbar>(R.id.toolbar)
-//         setSupportActionBar(toolbar)
-//
-//        val host: NavHostFragment = supportFragmentManager
-//            .findFragmentById(R.id.my_nav_host_fragment) as NavHostFragment? ?: return
-//
-//        // Set up Action Bar
-//        navController = host.navController
-//        drawerLayout = findViewById(R.id.drawer_layout)
-//
-//        appBarConfiguration = AppBarConfiguration(setOf(R.id.home_dest, R.id.settings_dest), drawerLayout)
-//        //setupActionBarWithNavController(navController, appBarConfiguration)
-//        setupActionBarWithNavController(navController, drawerLayout)
-//
-//        val navigationView = findViewById<NavigationView>(R.id.nav_view)
-//        navigationView.setupWithNavController(navController)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId === android.R.id.home) {
-            drawerLayout.openDrawer(Gravity.LEFT)
-            // return false;
-        }
-
-        // return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
-        return super.onOptionsItemSelected(item)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-//        return navController.navigateUp()
-//                || super.onSupportNavigateUp()
-        return super.onSupportNavigateUp()
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
     }
 
 }
